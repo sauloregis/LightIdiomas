a29159f1f50403812d1287531e88f847b495e8b4
sauloregis <sauloregis1@gmail.com>
2025-06-29 13:06:42 -0300

    primeiro

---
+++ Migrations/20250629152614_Primeira.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LightIdiomas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Primeira : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nm_Cliente = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Nacionalidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    RG = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Profissao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    Whatsapp = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
