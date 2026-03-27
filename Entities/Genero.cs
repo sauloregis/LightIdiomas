592e98f345e0e68a8268ea32ce46e955e3f1f59e
sauloregis <sauloregis1@gmail.com>
2026-03-26 21:16:19 -0300

    Ajustes display do Gênero

--- Entities/Genero.cs
+++ Entities/Genero.cs
@@ -1,10 +1,19 @@
-﻿namespace LightIdiomas.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LightIdiomas.Entities
 {
     public enum Genero
     {
+        [Display(Name = "Masculino")]
         Masculino,
+
+        [Display(Name = "Feminino")]
         Feminino,
+
+        [Display(Name = "Outro")]
         Outro,
+
+        [Display(Name = "Prefiro Não Informar")]
         PrefiroNaoInformar
     }
 }
